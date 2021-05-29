0ff97b8c5f0e20ff27d3ccbac1083fb79b404e06
naufalz6 <18119005@std.stei.itb.ac.id>
2021-05-29 15:21:53 +0700

    FDM Beres

--- tubes_medan.m
+++ tubes_medan.m
@@ -3,7 +3,7 @@ close all;
 clc;
 
 %segmentasi
-n=100;
+n=20;
 e_1=4*8.854*(10^(-12));
 e_2=e_1/4;
 x=zeros(n+1,1);
@@ -28,7 +28,7 @@ for i=1:n+1
     for j=1:n+1
         if x(j)<=2 && x(j)>=-2 && y(i)==0
             matrix(i,j)=-1;
-        elseif ((x(i))^2+(y(j))^2<=(100) && (x(i))^2+(y(j))^2>(10-20/n)^2)
+        elseif ((x(j))^2+(y(i))^2<=(100) && (x(j))^2+(y(i))^2>(10-20/n)^2)
             matrix(i,j)=1;
         end
     end
@@ -52,7 +52,7 @@ for i=1:n
         if ((x(j))^2+(y(i))^2<=(10-20/n)^2) && not(x(j)<=2 && x(j)>=-2 && y(i)==0)
             baris=baris+1;
             if matrix2(i-1,j)~= 0 && matrix2(i,j-1)~=0 && matrix2(i+1,j)~=0 && matrix2(i,j+1)~=0  
-                if x(i)>0 && x(i)<10 && y(j)==0
+                if x(j)>0 && x(j)<10 && y(i)==0
                     matrix3(baris,matrix2(i-1,j))=2*e_1;
                     matrix3(baris,matrix2(i+1,j))=2*e_2;
                     matrix3(baris,matrix2(i,j+1))=(e_1+e_2);
@@ -64,6 +64,12 @@ for i=1:n
                     matrix3(baris,matrix2(i-1,j))=(e_1+e_2);
                     matrix3(baris,matrix2(i+1,j))=(e_1+e_2);
                     matrix3(baris,matrix2(i,j))=-4*(e_1+e_2);
+                elseif y(i)==0 && x(j)==0
+                    matrix3(baris,matrix2(i,j-1))= (e_1+e_2) ;
+                    matrix3(baris,matrix2(i,j+1))= 2*e_1 ;
+                    matrix3(baris,matrix2(i+1,j))= (e_1+e_2) ;
+                    matrix3(baris,matrix2(i-1,j))= 2*e_1 ;
+                    matrix3(baris,matrix2(i,j))= 2*(2*e_1 + e_2) ;
                 else
                     matrix3(baris,matrix2(i,j+1))=1;
                     matrix3(baris,matrix2(i-1,j))=1;
@@ -72,87 +78,100 @@ for i=1:n
                     matrix3(baris,matrix2(i,j-1))=1;
                 end
             else
-                if matrix2(i,j-1)==0 && matrix2(i,j+1)==0 && matrix2(i-1,j)==0 && matrix2(i+1,j)~=0 %atas, kiri, kanan
+                
+                %Ketika ujungnya 1 V
+                if matrix(i,j-1)==1 && matrix(i,j+1)==1 && matrix(i-1,j)==1 && matrix2(i+1,j)~=0 %atas, kiri, kanan
                     matrix3(baris,matrix2(i+1,j))=(e_1+e_2);
                     matrix3(baris,matrix2(i,j))=-4*(e_1+e_2);
                     matrix4(matrix2(i,j),1)=-2*e_2*matrix(i,j-1)-(e_1+e_2)*matrix(i-1,j)-2*e_1*matrix(i,j+1);
-                elseif matrix2(i,j-1)==0 && matrix2(i,j+1)==0 && matrix2(i+1,j)==0 && matrix2(i-1,j)~=0 %bawah, kiri, kanan
+                elseif matrix(i,j-1)==1 && matrix(i,j+1)==1 && matrix(i+1,j)==1 && matrix2(i-1,j)~=0 %bawah, kiri, kanan
                     matrix3(baris,matrix2(i-1,j))=1;
                     matrix3(baris,matrix2(i,j))=-4;
                     matrix4(matrix2(i,j),1)=-1*matrix(i,j-1)-1*matrix(i+1,j)-1*matrix(i,j+1);
-                elseif matrix2(i,j-1)==0 && matrix2(i-1,j)==0 && matrix2(i+1,j)==0 && matrix2(i,j+1)~=0 %kiri, atas, bawah
+                elseif matrix(i,j-1)==1 && matrix(i-1,j)==1 && matrix(i+1,j)==1 && matrix2(i,j+1)~= 0 %kiri, atas, bawah
                     matrix3(baris,matrix2(i,j+1))=1;
                     matrix3(baris,matrix2(i,j))=-4;
                     matrix4(matrix2(i,j),1)=-1*matrix(i,j-1)-1*matrix(i+1,j)-1*matrix(i-1,j);
-                elseif matrix2(i,j+1)==0 && matrix2(i-1,j)==0 && matrix2(i+1,j)==0 && matrix2(i,j-1)~=0 %kanan, atas, bawah
+                elseif matrix(i,j+1)==1 && matrix(i-1,j)==1 && matrix(i+1,j)==1 && matrix2(i,j-1)~=0 %kanan, atas, bawah
                     matrix3(baris,matrix2(i,j-1))=(e_1+e_2);
                     matrix3(baris,matrix2(i,j))=-4*(e_1+e_2);
                     matrix4(matrix2(i,j),1)=-2*e_2*matrix(i-1,j)-2*e_1*matrix(i+1,j)-(e_1+e_2)*matrix(i,j+1);
-                elseif matrix2(i-1,j)==0 && matrix2(i,j-1)==0 && matrix2(i,j+1)~=0 && matrix2(i+1,j)~=0 %atas, kiri
+                elseif matrix(i-1,j)==1 && matrix(i,j-1)==1 && matrix2(i,j+1)~=0 && matrix2(i+1,j)~=0 %atas, kiri
                     matrix3(baris,matrix2(i,j))=-4;
                     matrix3(baris,matrix2(i,j+1))=1;
                     matrix3(baris,matrix2(i+1,j))=1;
                     matrix4(matrix2(i,j),1)=-1*matrix(i,j-1)-1*matrix(i-1,j);
-                elseif matrix2(i-1,j)==0 && matrix2(i,j+1)==0 && matrix2(i,j-1)~=0 && matrix2(i+1,j)~=0 %atas, kanan
+                elseif matrix(i-1,j)==1 && matrix(i,j+1)==1 && matrix2(i,j-1)~=0 && matrix2(i+1,j)~=0 %atas, kanan
                     matrix3(baris,matrix2(i,j))=-4;
                     matrix3(baris,matrix2(i+1,j))=1;
                     matrix3(baris,matrix2(i,j-1))=1;
                     matrix4(matrix2(i,j),1)=-1*matrix(i-1,j)-1*matrix(i,j+1);
-                elseif matrix2(i,j+1)==0 && matrix2(i+1,j)==0 && matrix2(i,j-1)~=0 && matrix2(i-1,j)~=0 %bawah, kanan
+                elseif matrix(i,j+1)==1 && matrix(i+1,j)==1 && matrix2(i,j-1)~=0 && matrix2(i-1,j)~=0 %bawah, kanan
                     matrix3(baris,matrix2(i,j))=-4;
                     matrix3(baris,matrix2(i,j-1))=1;
                     matrix3(baris,matrix2(i-1,j))=1;
                     matrix4(matrix2(i,j),1)=-1*matrix(i,j+1)-1*matrix(i+1,j);
-                elseif matrix2(i,j-1)==0 && matrix2(i+1,j)==0 && matrix2(i,j+1)~=0 && matrix2(i-1,j)~=0 %bawah, kiri
+                elseif matrix(i,j-1)==1 && matrix(i+1,j)==1 && matrix2(i,j+1)~=0 && matrix2(i-1,j)~=0 %bawah, kiri
                     matrix3(baris,matrix2(i,j))=-4;
                     matrix3(baris,matrix2(i,j+1))=1;
                     matrix3(baris,matrix2(i-1,j))=1;
                     matrix4(matrix2(i,j),1)=-1*matrix(i,j-1)-1*matrix(i+1,j);
-                elseif matrix2(i,j-1)==0 && matrix2(i,j+1)~=0 && matrix2(i-1,j)~=0 && matrix2(i+1,j)~=0 %kiri
-                    if y(j)==0
-                        matrix3(baris,matrix2(i-1,j))=2*e_1;
-                        matrix3(baris,matrix2(i+1,j))=2*e_2;
-                        matrix3(baris,matrix2(i,j+1))=(e_1+e_2);
-                        matrix3(baris,matrix2(i,j))=-4*(e_1+e_2);
-                        matrix4(matrix2(i,j),1)=-(e_1+e_2)*matrix(i,j-1);
-                    else
-                        matrix3(baris,matrix2(i,j))=-4;
-                        matrix3(baris,matrix2(i,j+1))=1;
-                        matrix3(baris,matrix2(i-1,j))=1;
-                        matrix3(baris,matrix2(i+1,j))=1;
-                        matrix4(matrix2(i,j),1)=-1*matrix(i,j-1);
-                    end
-                elseif matrix2(i,j+1)==0 && matrix2(i,j-1)~=0 && matrix2(i-1,j)~=0 && matrix2(i+1,j)~=0 %kanan
+                elseif matrix(i,j-1)==1 && matrix2(i,j+1)~=0 && matrix2(i-1,j)~=0 && matrix2(i+1,j)~=0 %kiri
+                    matrix3(baris,matrix2(i,j))=-4;
+                    matrix3(baris,matrix2(i,j+1))=1;
+                    matrix3(baris,matrix2(i-1,j))=1;
+                    matrix3(baris,matrix2(i+1,j))=1;
+                    matrix4(matrix2(i,j),1)=-1*matrix(i,j-1);
+                elseif matrix(i,j+1)==1 && matrix2(i,j-1)~=0 && matrix2(i-1,j)~=0 && matrix2(i+1,j)~=0 %kanan
                     matrix3(baris,matrix2(i,j))=-4;
                     matrix3(baris,matrix2(i,j-1))=1;
                     matrix3(baris,matrix2(i-1,j))=1;
                     matrix3(baris,matrix2(i+1,j))=1;
                     matrix4(matrix2(i,j),1)=-1*matrix(i,j+1);
-                elseif matrix2(i-1,j)==0 && matrix2(i,j-1)~=0 && matrix2(i,j+1)~=0 && matrix2(i+1,j)~=0 %atas
+                elseif matrix(i-1,j)==1 && matrix2(i,j-1)~=0 && matrix2(i,j+1)~=0 && matrix2(i+1,j)~=0 %atas
                     matrix3(baris,matrix2(i,j))=-4;
                     matrix3(baris,matrix2(i,j-1))=1;
                     matrix3(baris,matrix2(i,j+1))=1;
                     matrix3(baris,matrix2(i+1,j))=1;
                     matrix4(matrix2(i,j),1)=-1*matrix(i-1,j);
-                elseif matrix2(i+1,j)==0 && matrix2(i,j-1)~=0 && matrix2(i,j+1)~=0 && matrix2(i-1,j)~=0 %bawah
-                    if x(j)==0
-                        matrix3(baris,matrix2(i,j-1))=2*e_2;
-                        matrix3(baris,matrix2(i,j+1))=2*e_1;
-                        matrix3(baris,matrix2(i-1,j))=(e_1+e_2);
-                        matrix3(baris,matrix2(i,j))=-4*(e_1+e_2);
-                        matrix4(matrix2(i,j),1)=-(e_1+e_2)*matrix(i+1,j);
-                    else
-                        matrix3(baris,matrix2(i,j))=-4;
-                        matrix3(baris,matrix2(i,j-1))=1;
-                        matrix3(baris,matrix2(i,j+1))=1;
-                        matrix3(baris,matrix2(i-1,j))=1;
-                        matrix4(matrix2(i,j),1)=-1*matrix(i+1,j);
-                    end
+                elseif matrix(i+1,j)==1 && matrix2(i,j-1)~=0 && matrix2(i,j+1)~=0 && matrix2(i-1,j)~=0 %bawah
+                    matrix3(baris,matrix2(i,j))=-4;
+                    matrix3(baris,matrix2(i,j-1))=1;
+                    matrix3(baris,matrix2(i,j+1))=1;
+                    matrix3(baris,matrix2(i-1,j))=1;
+                    matrix4(matrix2(i,j),1)=-1*matrix(i+1,j);
+                    
+                %Ketika ujungnya -1 V    
+                elseif matrix(i,j-1)==-1 && matrix2(i,j+1)~=0 && matrix2(i-1,j)~=0 && matrix2(i+1,j)~=0 %kiri
+                    matrix3(baris,matrix2(i-1,j))=2*e_1;
+                    matrix3(baris,matrix2(i+1,j))=2*e_2;
+                    matrix3(baris,matrix2(i,j+1))=(e_1+e_2);
+                    matrix3(baris,matrix2(i,j))=-4*(e_1+e_2);
+                    matrix4(matrix2(i,j),1)=-1*(e_1+e_2)*matrix(i,j-1);
+                elseif matrix(i,j+1)==-1 && matrix2(i,j-1)~=0 && matrix2(i-1,j)~=0 && matrix2(i+1,j)~=0 %kanan
+                    matrix3(baris,matrix2(i,j))=-4;
+                    matrix3(baris,matrix2(i,j-1))=1;
+                    matrix3(baris,matrix2(i-1,j))=1;
+                    matrix3(baris,matrix2(i+1,j))=1;
+                    matrix4(matrix2(i,j),1)=-1*matrix(i,j+1);
+                elseif matrix(i-1,j)==-1 && matrix2(i,j-1)~=0 && matrix2(i,j+1)~=0 && matrix2(i+1,j)~=0 %atas
+                    matrix3(baris,matrix2(i,j))=-4;
+                    matrix3(baris,matrix2(i,j-1))=1;
+                    matrix3(baris,matrix2(i,j+1))=1;
+                    matrix3(baris,matrix2(i+1,j))=1;
+                    matrix4(matrix2(i,j),1)=-1*matrix(i-1,j);
+                elseif matrix(i+1,j)==-1 && matrix2(i,j-1)~=0 && matrix2(i,j+1)~=0 && matrix2(i-1,j)~=0 %bawah
+                    matrix3(baris,matrix2(i,j-1))=2*e_2;
+                    matrix3(baris,matrix2(i,j+1))=2*e_1;
+                    matrix3(baris,matrix2(i-1,j))=(e_1+e_2);
+                    matrix3(baris,matrix2(i,j))=-4*(e_1+e_2);
+                    matrix4(matrix2(i,j),1)=-(e_1+e_2)*matrix(i+1,j);
                 end
             end
         end
     end
 end
+
 matrix5=inv(matrix3)*matrix4;
 matrix6=matrix;
 sum=0;
@@ -164,6 +183,36 @@ for i=1:n+1
         end
     end
 end
+
+%mencari -q
+q=0;
+for i=1:n
+    for j=1:n
+        if i==((n/2)+1)-(n/10)-1 && j>=((n/2)+1-(n/10)-1) && j<=((n/2)+1+(n/10)+1) %atas
+            if j==(n/2)+1
+                q=q+(matrix6(i-1,j)-matrix6(i+1,j))*(e_1+e_2)/2;
+            elseif j<(n/2)+1
+                q=q+(matrix6(i-1,j)-matrix6(i+1,j))*e_1;
+            elseif j>(n/2)+1
+                q=q+(matrix6(i-1,j)-matrix6(i+1,j))*e_2;
+            end
+        elseif j==(n/2)+1-(n/10)-1 && i>=((n/2)+1-(n/10)-1) && i<=((n/2)+1+(n/10)+1) %kiri
+            q=q+(matrix6(i,j-1)-matrix6(i,j+1))*e_1;
+        elseif i==(n/2)+1+(n/10)+1 && j>=((n/2)+1-(n/10)-1) && j<=((n/2)+1+(n/10)+1) %bawah
+            q=q+(matrix6(i+1,j)-matrix6(i-1,j))*e_1;
+        elseif j==(n/2)+1+(n/10)+1 && i>=((n/2)+1-(n/10)-1) && i<=((n/2)+1+(n/10)+1) %kanan
+            if i==(n/2)+1
+                q=q+(matrix6(i,j+1)-matrix6(i,j-1))*(e_1+e_2)/2;
+            elseif i<(n/2)+1
+                q=q+(matrix6(i,j+1)-matrix6(i,j-1))*e_2;
+            elseif i>(n/2)+1
+                q=q+(matrix6(i,j+1)-matrix6(i,j-1))*e_1;
+            end
+        end
+    end
+end
+c=q/2;
+
 contourf(matrix6,'linecolor','non');
 colormap(jet(256));
 colorbar;
